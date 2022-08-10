c61672e1e3e295902a39091a81a2335bbb6c5254
noahhamann <noahhamann@10.0.0.70>
2022-08-10 12:12:07 -0700

    first commit

---
+++ CertificationProject/src/main/java/TestBase.java
@@ -0,0 +1,15 @@
+
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+	
+	URL driverPath = getClass().getResource("chromedriver 3");
+
+
+	
+
+}
